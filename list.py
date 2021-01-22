5e5d1b4ee8b4005862ec18eed30135e5e1e0f482
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-21 20:10:18 -0600

    counting commands

--- list.py
+++ list.py
@@ -4,11 +4,18 @@ import subprocess
 
 ###--- FUNCTIONS ---###
 def list_commands_and_create_txt():
-    all_commands = open('all_commands', 'w+')
+    all_commands = open('all_commands.txt', 'w+')
     subprocess.run('compgen -c', shell=True, stdout=all_commands)
     all_commands.close()
 
 
+def count_commands():
+    with open('all_commands.txt', 'r') as ac:
+        commands_list = [i for i in ac.read().splitlines()]
+        print(len(commands_list))
+
+
 ###--- DRIVER CODE ---###
 if __name__ == "__main__":
-    list_commands_and_create_txt()
+    # list_commands_and_create_txt()
+    count_commands()
