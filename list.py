7bd5f076b2efab0ff7207a5d93dd8e9a5be1c452
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-22 20:15:14 -0600

    added docstrings

--- list.py
+++ list.py
@@ -4,12 +4,19 @@ import subprocess
 
 ###--- FUNCTIONS ---###
 def list_commands_and_create_txt():
+    '''
+     lists all commands using running "compgen -c" command
+     in terminal, and "writes" output into .txt file
+    '''
     all_commands = open('all_commands.txt', 'w+')
     subprocess.run('compgen -c', shell=True, stdout=all_commands)
     all_commands.close()
 
 
 def count_commands():
+    '''
+     counts all commands obtained form terminal
+    '''
     with open('all_commands.txt', 'r') as all_commands_txt:
         commands_list = [
             command for command in all_commands_txt.read().splitlines()]
